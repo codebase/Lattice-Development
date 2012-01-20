4c0b3897d0dcb95542d73b7fba04c45d9d5180f7
Deepwinter <deepwinter@winterroot.net>
2012-01-20 14:28:14 -0500

    Moved cms default redirect after login to application config

--- application/config/auth.php
+++ application/config/auth.php
@@ -1,11 +1,13 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-$config['layout'] = 'LayoutLogin';
 
-return array(
+$config = array(
 	'driver'       => 'orm',
 	'hash_method'  => 'sha256',
 	'hash_key'     => 'asdfasdfaxxx9283r239f28h39fh29fhawpofiasfasdfq23rawefaw3raweasf',
 	'lifetime'     => 1209600,
 	'session_key'  => 'auth_user',
 );
+
+$config['layout'] = 'LayoutLogin';
+$config['redirect'] = 'cms';
